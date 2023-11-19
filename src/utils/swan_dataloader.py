c249caf86de52842559b7fc325233a672465bac4
Kyungbok Lee <dlrudqhr7180@gmail.com>
2023-11-19 15:56:56 -0500

    add load_data

--- src/utils/swan_dataloader.py
+++ src/utils/swan_dataloader.py
@@ -1,6 +1,7 @@
 import os
 
 import pandas as pd
+import torchaudio
 from torch.utils.data import DataLoader, Dataset
 
 from utils import DatasetCreater
@@ -16,25 +17,40 @@ CSV_PATH = "./utils/swan_df.csv"
 
 
 class SwanDataset(Dataset):
-    def __init__(self, dataset_type, transform=None):
+    def __init__(self, dataset_type: str, transform=None):
         if dataset_type not in ["train", "test1", "test2", "test3"]:
             raise ValueError("Invalid dataset type")
-        
-        if not os.path.exists(CSV_PATH):
-            print("did not find csv file..")
-            print("creating csv file..")
-            DatasetCreater()
-        
-        self.df = self.get_df(dataset_type)
+
+        self.load_data(dataset_type=dataset_type)
             
-    def get_df(self, dataset_type):
-        df = pd.read_csv(CSV_PATH)
-        return df[df["group"] == dataset_type]
         
 
     def __len__(self):
         return len(self.df)
 
+
     def __getitem__(self, idx):
         # implement audio and visual
-        pass
+        row = self.df.iloc[idx]
+        audio_dir = row["audio_dir"]
+        waveform, _  = torchaudio.load(audio_dir)
+
+        target = row["target"]
+        
+        return waveform, target
+
+
+
+    def load_data(self, dataset_type:str):
+        if not os.path.exists(CSV_PATH):
+            print("did not find csv file..")
+            print("creating csv file..")
+            DatasetCreater()
+
+        self.df = self.get_df(dataset_type)
+
+
+
+    def get_df(self, dataset_type:str):
+        df = pd.read_csv(CSV_PATH)
+        return df[df["group"] == dataset_type]
