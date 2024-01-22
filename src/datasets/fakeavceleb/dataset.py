af514e2321e8cf2a2a418b9dbd5303ec824629e8
Kyungbok Lee <dlrudqhr7180@gmail.com>
2024-01-22 17:24:55 -0500

    add dataset type

--- src/datasets/fakeavceleb/dataset.py
+++ src/datasets/fakeavceleb/dataset.py
@@ -1,8 +1,8 @@
 import json
 import os
 import sys
-from multiprocessing import Pool
 
+import cv2
 import numpy as np
 import pandas as pd
 import torch
@@ -12,9 +12,20 @@ import torchvision.transforms as T
 from PIL import Image
 from torch.utils.data import Dataset
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+
+sys.path.append(src_path)
+sys.path.append(os.path.join(src_path, "models/av_hubert/avhubert"))
+# sys.path.append(os.path.join(src_path, "models/av_hubert"))
+
+import logging
+
+import utils as avhubert_utils
+
 from data.augmentation import *
-from utils.constants import FAKE, ORIGINAL
+from util.constants import FAKE, ORIGINAL
+
+logging.getLogger("PIL.PngImagePlugin").setLevel(logging.CRITICAL + 1)
 
 
 def pil_loader(path):
@@ -24,10 +35,16 @@ def pil_loader(path):
 
 
 class FakeAVDataset(Dataset):
-    def __init__(self, frame_num, classifier_type, dataset_type):
+    def __init__(
+        self,
+        dataset_type,
+        frame_num,
+        classifier_type,
+    ):
         # load data
         self._load_data(classifier_type, dataset_type)
 
+        self.classifier_type = classifier_type
         # set target frame number
         self.video_target_frames = frame_num
         self.audio_target_frames = frame_num * 4
@@ -37,21 +54,32 @@ class FakeAVDataset(Dataset):
             win_length=int(16000 * 0.025),
             hop_length=int(16000 * 0.01),
             center=False,
-            n_mels=80,
+            n_mels=26,
         )
 
         self.video_transform = transforms.Compose([Scale(size=(224, 224)), ToTensor(), Normalize()])
+
+        lip_crop_size = 88
+        lip_mean = 0.421
+        lip_std = 0.165
+        self.lip_transform = avhubert_utils.Compose(
+            [
+                avhubert_utils.Normalize(0.0, 255.0),
+                avhubert_utils.CenterCrop((lip_crop_size, lip_crop_size)),
+                avhubert_utils.Normalize(lip_mean, lip_std),
+            ]
+        )
 
     def _load_data(self, classifier_type: str, dataset_type: str):
         """
         Load and preprocess data from csv file
         Args:
-            classifier_type (str): V, A, AV
+            classifier_type (str): V, A, AV, ALL
             dataset_type (str): train, test
         """
 
         # check input values
-        if classifier_type not in ["V", "A", "AV"]:
+        if classifier_type not in ["V", "A", "AV", "ALL"]:
             raise ValueError("classifier_type should be one of V, A, AV")
         if dataset_type not in ["train", "test"]:
             raise ValueError("dataset_type should be one of train, test")
@@ -80,6 +108,9 @@ class FakeAVDataset(Dataset):
 
         self.df = get_labeled_df(classifier_type, self.df)
 
+        # retrict dataset to 1000 samples
+        # self.df = self.df[:100]
+
         fake_count = len(self.df[self.df["label"] == FAKE])
         real_count = len(self.df[self.df["label"] == ORIGINAL])
 
@@ -89,13 +120,25 @@ class FakeAVDataset(Dataset):
 
         # set label based on classifier type
 
+    def stacker(self, feats, stack_order):
+        """
+        Concatenating consecutive audio frames
+        Args:
+        feats - numpy.ndarray of shape [T, F]
+        stack_order - int (number of neighboring frames to concatenate
+        Returns:
+        feats - numpy.ndarray of shape [T', F']
+        """
+        feat_dim = feats.shape[1]
+        if len(feats) % stack_order != 0:
+            res = stack_order - len(feats) % stack_order
+            res = np.zeros([res, feat_dim]).astype(np.float32)
+            feats = np.concatenate([feats, res], axis=0)
+        feats = feats.reshape((-1, stack_order, feat_dim)).reshape(-1, stack_order * feat_dim)
+        return feats
+
     def __getitem__(self, index):
         row = self.df.iloc[index]
-
-        directory = row["preprocessed_directory"]
-
-        frames = self._load_frames(directory)
-        spectrogram = self._load_audio(directory)
 
         # get label
         category = row["category"]
@@ -104,17 +147,45 @@ class FakeAVDataset(Dataset):
         else:
             label = FAKE
 
+        directory = row["preprocessed_directory"]
+        if self.classifier_type == "V":
+            frames = self._load_frames(directory)  # [B, T, C, H, W]
+            return frames, label
+
+        elif self.classifier_type == "AV":
+            lip_frames = self._load_lip_frames(directory)  # [B, C, T, H, W]
+            spectrogram = self._load_audio(directory)  # [B ,T, F]
+            stacked_spectrogram = self.stacker(spectrogram, 4)  # [B, T, F]
+            stacked_spectrogram = stacked_spectrogram.transpose(1, 0)  # [B, F, T]
+            return lip_frames, stacked_spectrogram, label
+        elif self.classifier_type == "A":
+            spectrogram = self._load_audio(directory)
+            return spectrogram, label
+        elif self.classifier_type == "ALL":
+            frames = self._load_frames(directory)
+            lip_frames = self._load_lip_frames(directory)
+            spectrogram = self._load_audio(directory)
+            stacked_spectrogram = self.stacker(spectrogram, 4)
+            stacked_spectrogram = stacked_spectrogram.transpose(1, 0)
+            return frames, lip_frames, stacked_spectrogram, label
+
         # return spectrogram, label
-        return frames, spectrogram, label
+        return frames, lip_frames, spectrogram, stacked_spectrogram, label
 
     def _load_frames(self, directory: str):
+        """
+        Load frames from directory
+        Args:
+            directory (str): directory path
+
+        Returns:
+            t_seq: torch.tensor, [B, T, C, H, W]
+        """
         # load fixed number of frames
         frame_dir = os.path.join(directory, "frames")
 
         # frame_0.jpg -> 0
-        sorted_frames_files = sorted(
-            os.listdir(frame_dir), key=lambda x: int(x.split(".")[0].split("_")[1])
-        )
+        sorted_frames_files = sorted(os.listdir(frame_dir), key=lambda x: int(x.split(".")[0].split("_")[1]))
         frame_num = len(sorted_frames_files)
 
         # clip frames to target number
@@ -130,6 +201,49 @@ class FakeAVDataset(Dataset):
         if frame_num < self.video_target_frames:
             p = self.video_target_frames - frame_num
             t_seq = torch.nn.functional.pad(t_seq, (0, 0, 0, 0, 0, 0, 0, p))
+        return t_seq
+
+    def _load_lip_frames(self, directory: str):
+        """
+        Load lip frames from directory
+        Args:
+            directory (str): directory path
+
+        Returns:
+            t_seq: torch.tensor, [B, C, T, H, W]
+        """
+
+        def pil_loader_gray(path):
+            with open(path, "rb") as f:
+                img = Image.open(f)
+                img = np.array(img)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                return img
+
+        # load fixed number of frames
+        lip_frame_dir = os.path.join(directory, "lip")
+
+        # frame_0.jpg -> 0
+        sorted_frames_files = sorted(
+            os.listdir(lip_frame_dir), key=lambda x: int(x.split(".")[0].split("_")[1])
+        )
+        frame_num = len(sorted_frames_files)
+
+        # clip frames to target number
+        if frame_num > self.video_target_frames:
+            sorted_frames_files = sorted_frames_files[: self.video_target_frames]
+
+        # load and transform frames
+        seq = [pil_loader_gray(os.path.join(lip_frame_dir, img)) for img in sorted_frames_files]
+        seq = np.stack(seq)
+        t_seq = self.lip_transform(seq)
+        t_seq = torch.tensor(t_seq, dtype=torch.float32).unsqueeze(0)
+
+        # pad frames to target number
+        if frame_num < self.video_target_frames:
+            p = self.video_target_frames - frame_num
+            t_seq = torch.nn.functional.pad(t_seq, (0, 0, 0, 0, 0, 0, 0, p))
+
         return t_seq
 
     def _load_audio(self, audio_dir: str):
@@ -149,8 +263,16 @@ class FakeAVDataset(Dataset):
             mel_spectrogram = m(mel_spectrogram)
         elif p < 0:
             mel_spectrogram = mel_spectrogram[:, 0 : self.audio_target_frames]
-        mel_spectrogram = torch.transpose(mel_spectrogram, 0, 1)
-        # [time_frame_num, frequency_bins], ex) [120, 80]
+
+        # [F, T] => [T, F]
+        mel_spectrogram = mel_spectrogram.transpose(1, 0)
+        # mel_spectrogram = mel_spectrogram.numpy().transpose(1, 0)
+        # # stack audio frames to better align with video frames
+        # feats = stacker(mel_spectrogram, 4)
+
+        # feats = feats.transpose(1, 0)
+        # print(f"feat shape: {feats.shape}")
+
         return mel_spectrogram
 
     def __len__(self):
@@ -195,9 +317,7 @@ class AudiosetDataset(Dataset):
         self.audio_conf = {
             "num_mel_bins": 80,
         }
-        print(
-            "---------------the {:s} dataloader---------------".format(self.audio_conf.get("mode"))
-        )
+        print("---------------the {:s} dataloader---------------".format(self.audio_conf.get("mode")))
         self.melbins = self.audio_conf.get("num_mel_bins")
         self.freqm = self.audio_conf.get("freqm")
         self.timem = self.audio_conf.get("timem")
@@ -301,3 +421,21 @@ if __name__ == "__main__":
     for classifier_type in classifier_types:
         for dataset_type in dataset_types:
             dataset = FakeAVDataset(30, classifier_type, dataset_type)
+    # result
+    # Dataset config : V, train
+    # fake: 17674, real: 860
+
+    # Dataset config : V, test
+    # fake: 2892, real: 140
+
+    # Dataset config : A, train
+    # fake: 9767, real: 8767
+
+    # Dataset config : A, test
+    # fake: 1590, real: 1442
+
+    # Dataset config : AV, train
+    # fake: 18104, real: 430
+
+    # Dataset config : AV, test
+    # fake: 2962, real: 70
