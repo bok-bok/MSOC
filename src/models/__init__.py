85ebc7952234256b31813372aa3604d6f0813f6d
Kyungbok Lee <dlrudqhr7180@gmail.com>
2024-01-23 19:02:46 -0500

    add some models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,64 @@
+import os
+import sys
+
+# import fairseq
+# import hubert
+# import hubert_asr
+# import hubert_pretraining
+import torch
+
 from models.ast_models import ASTModel
+
+# from models.av_model import AVModel
 from models.vivit import ViViT
+from util.constants import (
+    HUBERT_ENCODER_MODEL_STATE_DICT_PATH,
+    MC_TCN_MODEL_PATH,
+    MODEL_WEIGHTS_PATH,
+)
+
+# sys.path.append("./av_hubert")
+
+
+# def load_av_model(pretrained: bool, device: str):
+#     model = AVModel(pretrained=pretrained, device=device)
+#     return model
+
+
+def load_ast(label_dim: int, input_fdim: int, input_tdim: int, pretrained: bool, device: str):
+    if pretrained:
+        model_name = f"ast_model_{label_dim}_{input_fdim}_{input_tdim}.pth"
+        model_path = os.path.join(MODEL_WEIGHTS_PATH, model_name)
+        print(f"Loading AST model from {model_path}")
+        if not os.path.exists(model_path):
+            raise Exception(f"Model {model_name} does not exist")
+        model = torch.load(model_path)
+
+        return model.to(device)
+    else:
+        return ASTModel(label_dim=label_dim, input_fdim=input_fdim, input_tdim=input_tdim).to(device)
+
+
+def load_vivit(
+    image_size: int,
+    patch_size: int,
+    num_classes: int,
+    num_frames: int,
+    pretrained: bool,
+    device: str,
+):
+    if pretrained:
+        model_name = f"vivit_model_{image_size}_{patch_size}_{num_classes}_{num_frames}.pth"
+        model_path = os.path.join(MODEL_WEIGHTS_PATH, model_name)
+        print(f"Loading ViViT model from {model_path}")
+        if not os.path.exists(model_path):
+            raise Exception(f"Model {model_name} does not exist")
+        model = torch.load(model_path)
+        return model.to(device)
+    else:
+        return ViViT(
+            image_size=image_size,
+            patch_size=patch_size,
+            num_classes=num_classes,
+            num_frames=num_frames,
+        ).to(device)
--- src/models/model.py
+++ src/models/model.py
@@ -1,17 +1,36 @@
+import numpy as np
 import torch
 from torch import nn
 
-from models import ASTModel, ViViT
+from models import load_ast, load_av_model, load_vivit
 
 
-class AVModel(nn.Module):
-    def __init__(self, frame_num=30):
-        super(AVModel, self).__init__()
+class EnsembleModel(nn.Module):
+    def __init__(
+        self, decision_type: str, frame_num: int = 30, pretrained: bool = False, device: str = "cuda:1"
+    ):
+        super(EnsembleModel, self).__init__()
+        if decision_type not in ["mv", "asf", "sf", "ff"]:
+            """
+            mv: majority voting
+            asf: average score fusion
+            sf: score fusion
+            ff: feature fusion
+            """
+            raise Exception(f"Invalid decision type {decision_type}")
+
+        self.decision_type = decision_type
+        self.device = device
+        self.softmax = nn.Softmax(dim=1)
+
+        # decision model config
+        self.asf_threshold = 0.5
+
         # general config
         output_dim = 2
 
         # audio config
-        input_fdim = 80
+        input_fdim = 26
         input_tdim = frame_num * 4
 
         # video config
@@ -19,31 +38,110 @@ class AVModel(nn.Module):
         patch_size = 16
 
         # init model
-        self.audio_model = ASTModel(
-            label_dim=output_dim, input_fdim=input_fdim, input_tdim=input_tdim
-        )
-        self.video_model = ViViT(
-            image_size=frame_size,
-            patch_size=patch_size,
-            num_classes=output_dim,
-            num_frames=frame_num,
+        self.audio_model = load_ast(output_dim, input_fdim, input_tdim, pretrained=pretrained, device=device)
+        self.video_model = load_vivit(
+            frame_size, patch_size, output_dim, frame_num, pretrained=pretrained, device=device
         )
 
-    def forward(self, frames, spectrogram):
+        self.audio_visual_model = load_av_model(pretrained=pretrained, device=device)
+
+    def forward(self, frames, lip_frames, spectrogram, stacked_spectrogram):
         # audio
-        # output dim : [batch, 2] feature dim : [batch, 768]
+        # input dim : [B, T, F]
+        # output dim : [B, 2] feature dim : [B, 768]
         audio_output, audio_feature = self.audio_model(spectrogram)
 
         # video
-        # output dim : [batch, 2] feature dim : [batch, 192]
+        # input dim : [B, T, C, H, W]
+        # output dim : [B, 2] feature dim : [B, 192]
         video_output, video_feature = self.video_model(frames)
 
-        # concat
-        concated_feature = torch.cat((audio_feature, video_feature), dim=1)
+        # audio visual
+        # audio input dim : [B, F, T]
+        # video input dim : [B, C, T, H, W]
+        # feature dim : [B, 768]
+        print(f"Lip frames shape: {lip_frames.shape}")
+        print(f"Stacked spectrogram shape: {stacked_spectrogram.shape}")
+        audio_visual_output, audio_visual_feature = self.audio_visual_model(lip_frames, stacked_spectrogram)
 
-        output = self.decision_module(audio_output, video_output, concated_feature)
-        return output
+        print(f"Audio feature shape: {audio_feature.shape}")
+        print(f"Audio output shape: {audio_output.shape}")
+        print(audio_output)
 
-    def decision_module(self, audio_output, video_output, concated_feature):
-        # do something here
-        pass
+        print(f"Video feature shape: {video_feature.shape}")
+        print(f"Video output shape: {video_output.shape}")
+        print(video_output)
+
+        print(f"Audio Visual feature shape: {audio_visual_feature.shape}")
+        print(f"Audio Visual output shape: {audio_visual_output.shape}")
+        print(audio_visual_output)
+
+        audio_logit = self.softmax(audio_output)
+        video_logit = self.softmax(video_output)
+        audio_visual_logit = self.softmax(audio_visual_output)
+
+        audio_score = audio_logit.argmax(dim=1)
+        video_score = video_logit.argmax(dim=1)
+        audio_visual_score = audio_visual_logit.argmax(dim=1)
+
+        if self.decision_type == "mv":
+            return self.majority_voting_decision_module(audio_score, video_score, audio_visual_score)
+        elif self.decision_type == "asf":
+            return self.average_score_fusion_decision_module(audio_score, video_score, audio_visual_score)
+
+    def majority_voting_decision_module(self, audio_score, video_score, audio_visual_score):
+        """
+        Majority voting decision module
+        Args:
+        audio_score - torch.Tensor of shape [B]
+        video_score - torch.Tensor of shape [B]
+        audio_visual_score - torch.Tensor of shape [B]
+        Returns:
+        score - torch.Tensor of shape [B]
+        """
+
+        score = torch.stack([audio_score, video_score, audio_visual_score], dim=1)
+        score = torch.mode(score, dim=1)[0]
+        return score
+
+    def average_score_fusion_decision_module(self, audio_score, video_score, audio_visual_score):
+        """
+        Average score fusion decision module
+        Args:
+        audio_score - torch.Tensor of shape [B]
+        video_score - torch.Tensor of shape [B]
+        audio_visual_score - torch.Tensor of shape [B]
+        Returns:
+        score - torch.Tensor of shape [B]
+        """
+        score = (audio_score + video_score + audio_visual_score) / 3
+        score = 1 if score >= self.asf_threshold else 0
+        score = (score > self.asf_threshold).float()
+        return score
+
+    def create_checkpoint(self, epoch, optimizer, scheduler, loss, path):
+        checkpoint = {
+            "epoch": epoch,
+            "model_state_dict": self.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "scheduler_state_dict": scheduler.state_dict(),
+            "loss": loss,
+        }
+        torch.save(checkpoint, path)
+
+    def stacker(feats, stack_order):
+        """
+        Concatenating consecutive audio frames
+        Args:
+        feats - numpy.ndarray of shape [T, F]
+        stack_order - int (number of neighboring frames to concatenate
+        Returns:
+        feats - numpy.ndarray of shape [T', F']
+        """
+        feat_dim = feats.shape[1]
+        if len(feats) % stack_order != 0:
+            res = stack_order - len(feats) % stack_order
+            res = np.zeros([res, feat_dim]).astype(np.float32)
+            feats = np.concatenate([feats, res], axis=0)
+        feats = feats.reshape((-1, stack_order, feat_dim)).reshape(-1, stack_order * feat_dim)
+        return feats
