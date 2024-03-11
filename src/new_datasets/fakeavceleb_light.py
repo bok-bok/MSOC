61d089dc855ad04774f1619ec6b0817c01abb89a
Kyungbok Lee <dlrudqhr7180@gmail.com>
2024-03-10 21:15:58 -0400

    add new dataset

--- src/new_datasets/fakeavceleb_light.py
+++ src/new_datasets/fakeavceleb_light.py
@@ -3,15 +3,15 @@ import random
 from dataclasses import dataclass
 from typing import List, Optional, Tuple, Union
 
+import librosa
 import numpy as np
 import pandas as pd
 import torch
-
-# from lightning import LightningDataModule
+import torchaudio.transforms as a_T
+from audiomentations import AddGaussianNoise, Compose, PitchShift, Shift, TimeStretch
 from python_speech_features import logfbank
 from pytorch_lightning import LightningDataModule
-
-# from lightning.utils.types import T_LABEL
+from pytorch_lightning.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
 from scipy.io import wavfile
 from torch import Tensor
 from torch.nn import functional as F
@@ -19,8 +19,6 @@ from torch.utils.data import DataLoader, Dataset, RandomSampler
 from torch.utils.data.sampler import WeightedRandomSampler
 
 import models.avhubert.utils as custom_utils
-
-# from pytorch_lightning.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
 
 
 @dataclass
@@ -35,7 +33,6 @@ class Metadata:
     gender: str
     vid: str
     path: str
-    # split: str
 
 
 dtype = {
@@ -49,7 +46,6 @@ dtype = {
     "gender": str,
     "vid": str,
     "path": str,
-    # "split": str,
 }
 
 T_LABEL = Union[Tensor, Tuple[Tensor, Tensor, Tensor]]
@@ -67,7 +63,13 @@ g.manual_seed(0)
 
 class Fakeavceleb(Dataset):
 
-    def __init__(self, subset: str, root: str = "data", metadata: Optional[List[Metadata]] = None):
+    def __init__(
+        self,
+        subset: str,
+        root: str = "data",
+        metadata: Optional[List[Metadata]] = None,
+        augmentation: bool = False,
+    ):
         self.subset = subset
         self.root = root
 
@@ -78,6 +80,14 @@ class Fakeavceleb(Dataset):
         self.stack_order_audio = 4
         self.pad_audio = False
         self.scale_percent = 0.5
+        self.augmenation = augmentation
+        self.audio_augment = Compose(
+            [
+                # TimeStretch(min_rate=0.8, max_rate=1.25, p=0.5, leave_length_unchanged=True),
+                Shift(min_shift=-0.1, max_shift=0.1, rollover=True, p=1),
+            ]
+        )
+        print(f"augmentation: {self.augmenation}")
 
         self.metadata = metadata
 
@@ -90,6 +100,7 @@ class Fakeavceleb(Dataset):
                     custom_utils.Normalize(self.image_mean, self.image_std),
                 ]
             )
+
         else:
             self.transform = custom_utils.Compose(
                 [
@@ -115,10 +126,17 @@ class Fakeavceleb(Dataset):
             mm_label = 2
         else:
             mm_label = 3
-
+        if meta.category == "B":
+            s_label = 0  # real_video-fake_audio
+        else:
+            s_label = 1
         path = "/".join(meta.path.split("/")[1:])
         file_path = os.path.join(self.root, path, meta.vid)
-        video, audio = self.load_feature(file_path)
+        video, audio, modified = self.load_feature(file_path)
+
+        if modified:
+            s_label = 0
+
         audio, video = torch.from_numpy(audio.astype(np.float32)) if audio is not None else None, (
             torch.from_numpy(video.astype(np.float32)) if video is not None else None
         )
@@ -136,6 +154,7 @@ class Fakeavceleb(Dataset):
             "c_label": torch.tensor(c_label),
             "m_label": torch.tensor(m_label),
             "mm_label": torch.tensor(mm_label),
+            "s_label": torch.tensor(s_label),
         }
 
     def __len__(self) -> int:
@@ -173,15 +192,26 @@ class Fakeavceleb(Dataset):
             return feats
 
         video_fn = mix_name
+        modified = False
         # video_fn = video_fn.replace(" (", "-")
         # video_fn = video_fn.replace(")", "")
         audio_fn = video_fn.replace(".mp4", ".wav")
 
         video_feats = self.load_video(video_fn)  # [T, H, W, 1]
+
         sample_rate, wav_data = wavfile.read(audio_fn)
         wav_data = wav_data[:, 0]
+        wav_data = wav_data.astype(np.float32)
+
+        if self.augmenation and self.subset in "train":
+            p = 0.5
+            if random.random() < p:
+                wav_data = self.audio_augment(samples=wav_data, sample_rate=16000)
+                modified = True
+
         assert sample_rate == 16_000 and len(wav_data.shape) == 1
         audio_feats = logfbank(wav_data, samplerate=sample_rate).astype(np.float32)  # [T, F]
+
         audio_feats = stacker(
             audio_feats, self.stack_order_audio
         )  # [T/stack_order_audio, F*stack_order_audio]
@@ -194,7 +224,8 @@ class Fakeavceleb(Dataset):
                 )
             elif diff > 0:
                 audio_feats = audio_feats[:-diff]
-        return video_feats, audio_feats
+
+        return video_feats, audio_feats, modified
 
 
 class FakeavcelebDataModule(LightningDataModule):
@@ -213,10 +244,11 @@ class FakeavcelebDataModule(LightningDataModule):
         take_train: int = None,
         take_dev: int = None,
         take_test: int = None,
+        augmentation: bool = False,
+        dataset_type: str = "original",
     ):
         print("batch_size", batch_size)
         print("num_workers", num_workers)
-        print(f"max_sample_size: {max_sample_size}")
         super().__init__()
         self.root = root
         self.batch_size = batch_size
@@ -229,65 +261,87 @@ class FakeavcelebDataModule(LightningDataModule):
         self.random_crop = False
         self.max_sample_size = max_sample_size
         self.train_fold = train_fold
+        self.augmentation = augmentation
+        self.dataset_type = dataset_type
 
     def setup(self, stage: Optional[str] = None) -> None:
-        self.metadata = pd.read_csv(os.path.join(self.root, "meta_data.csv"))  # .loc
-        self.metadata.columns = dtype.keys()
-
+        self.metadata = pd.read_csv(os.path.join(self.root, "meta_data.csv"), dtype=dtype)  # .loc
         print(self.root, self.train_fold)
-        # self.train_fold = os.path.join(self.root, self.train_fold)
+        self.metadata.columns = dtype.keys()
+        if self.dataset_type == "original":
+            self.set_original_dataset()
+        elif self.dataset_type == "new":
+            self.set_new_dataset()
 
-        # print(self.train_fold)
-
-        # train_f = open(self.train_fold)
-        # train_line = train_f.readline()
-        # train_ids = []
-        # while train_line:
-        #     train_line = train_f.readline()[:7]
-        #     if "id" in train_line:
-        #         train_ids.append(train_line)
-        # train_f.close()
-        method_column_mapping = {
-            "wav2lip": "source",
-            "rtvc": "source",
-            "faceswap": "target1",
-            "fsgan": "target1",
-            "faceswap-wav2lip": "target1",
-            "fsgan-wav2lip": "target1",
-            "real": "source",
-        }
-        self.metadata["subject"] = self.metadata.apply(
-            lambda row: row[method_column_mapping[row["method"]]], axis=1
+    def set_new_dataset(self):
+        print("new dataset")
+        df = self.metadata
+        train_df_A = df[df["category"] == "A"].sample(400, random_state=42)
+        train_df_B = df[df["category"] == "B"].sample(400, random_state=42)
+        train_df_C = (
+            df[(df["category"] == "C") & (df["method"] != "faceswap")]
+            .groupby("method")
+            .sample(n=200, random_state=42)
         )
-        subjects = self.metadata["source"].unique()
-        train_ids = subjects[:400]
+        train_df_D = (
+            df[(df["category"] == "D") & (df["method"] != "faceswap-wav2lip")]
+            .groupby("method")
+            .sample(n=200, random_state=42)
+        )
+        self.train_metadata = pd.concat([train_df_A, train_df_B, train_df_C, train_df_D])
 
-        # train_metadata = self.metadata[self.metadata["source"].isin(train_ids)]
-        # test_metadata = self.metadata[~self.metadata["source"].isin(train_ids)]
-        train_metadata = self.metadata[self.metadata["subject"].isin(train_ids)]
-        test_metadata = self.metadata[~self.metadata["subject"].isin(train_ids)]
-        train_count = 400
-        test_count = 100
-        train_metadata_A = train_metadata[train_metadata["category"].isin(["A"])][:train_count]
-        train_metadata_B = train_metadata[train_metadata["category"].isin(["B"])][:train_count]
-        train_metadata_C = train_metadata[train_metadata["category"].isin(["C"])][:train_count]
-        train_metadata_D = train_metadata[train_metadata["category"].isin(["D"])][:train_count]
+        test_df_A = df[df["category"] == "A"].drop(train_df_A.index)
+        # test_df_B = df[df["category"] == "B"].drop(train_df_B.index)
+        test_df_C = df[(df["category"] == "C") & (df["method"] == "faceswap")].sample(n=100)
+        test_df_D = df[(df["category"] == "D") & (df["method"] == "faceswap-wav2lip")].sample(n=100)
+        self.test_metadata = pd.concat([test_df_A, test_df_C, test_df_D])
+
+        self.train_dataset = self.Dataset(
+            "train", self.root, metadata=self.train_metadata, augmentation=self.augmentation
+        )
+        self.test_dataset = self.Dataset(
+            "test", self.root, metadata=self.test_metadata, augmentation=self.augmentation
+        )
+
+    def set_original_dataset(self):
+        self.train_fold = os.path.join(self.root, self.train_fold)
+
+        print(self.train_fold)
+
+        with open(self.train_fold) as train_f:
+            train_ids = []
+            for train_line in train_f:
+                train_id = train_line[:7]
+                if "id" in train_id:
+                    train_ids.append(train_id)
+
+        train_metadata = self.metadata[self.metadata["source"].isin(train_ids)]
+        test_metadata = self.metadata[~self.metadata["source"].isin(train_ids)]
+
+        train_metadata_A = train_metadata[train_metadata["category"].isin(["A"])][:400]
+        train_metadata_B = train_metadata[train_metadata["category"].isin(["B"])][:400]
+        train_metadata_C = train_metadata[train_metadata["category"].isin(["C"])][:400]
+        train_metadata_D = train_metadata[train_metadata["category"].isin(["D"])][:400]
 
         self.train_metadata = pd.concat(
             [train_metadata_A, train_metadata_B, train_metadata_C, train_metadata_D]
         )
         # self.train_metadata = train_metadata
         #
-        test_metadata_A = test_metadata[test_metadata["category"].isin(["A"])][:test_count]
-        test_metadata_B = test_metadata[test_metadata["category"].isin(["B"])][:test_count]
-        test_metadata_C = test_metadata[test_metadata["category"].isin(["C"])][:test_count]
-        test_metadata_D = test_metadata[test_metadata["category"].isin(["D"])][:test_count]
+        test_metadata_A = test_metadata[test_metadata["category"].isin(["A"])][:100]
+        test_metadata_B = test_metadata[test_metadata["category"].isin(["B"])][:100]
+        test_metadata_C = test_metadata[test_metadata["category"].isin(["C"])][:100]
+        test_metadata_D = test_metadata[test_metadata["category"].isin(["D"])][:100]
         #
         self.test_metadata = pd.concat([test_metadata_A, test_metadata_B, test_metadata_C, test_metadata_D])
         # self.test_metadata = test_metadata
 
-        self.train_dataset = self.Dataset("train", self.root, metadata=self.train_metadata)
-        self.test_dataset = self.Dataset("test", self.root, metadata=self.test_metadata)
+        self.train_dataset = self.Dataset(
+            "train", self.root, metadata=self.train_metadata, augmentation=self.augmentation
+        )
+        self.test_dataset = self.Dataset(
+            "test", self.root, metadata=self.test_metadata, augmentation=self.augmentation
+        )
 
     def crop_to_max_size(self, wav, target_size, start=None):
         size = len(wav)
@@ -342,6 +396,7 @@ class FakeavcelebDataModule(LightningDataModule):
             "c_label": torch.LongTensor([s["c_label"] for s in samples]),
             "m_label": torch.LongTensor([s["m_label"] for s in samples]),
             "mm_label": torch.LongTensor([s["mm_label"] for s in samples]),
+            "s_label": torch.LongTensor([s["s_label"] for s in samples]),
         }
 
         return batch
@@ -372,7 +427,7 @@ class FakeavcelebDataModule(LightningDataModule):
             ).contiguous()  # [B, T, H, W, C] -> [B, C, T, H, W]
         return collated_audios, padding_mask, audio_starts
 
-    def train_dataloader(self):
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
         return DataLoader(
             self.train_dataset,
             batch_size=self.batch_size,
@@ -389,7 +444,7 @@ class FakeavcelebDataModule(LightningDataModule):
     #                       collate_fn=self.collater, sampler = WeightedRandomSampler(weights, num_samples=30000, replacement=True),
     #                       worker_init_fn=seed_worker, generator=g)
     #
-    def val_dataloader(self):
+    def val_dataloader(self) -> EVAL_DATALOADERS:
         return DataLoader(
             self.test_dataset,
             batch_size=self.batch_size,
@@ -400,7 +455,7 @@ class FakeavcelebDataModule(LightningDataModule):
             generator=g,
         )  # , worker_init_fn=seed_worker, generator=g
 
-    def test_dataloader(self):
+    def test_dataloader(self) -> EVAL_DATALOADERS:
         return DataLoader(
             self.test_dataset,
             batch_size=self.batch_size,
