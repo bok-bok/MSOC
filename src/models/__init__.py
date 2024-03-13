9ce184431c87a40301294cf460ddd23bf7e4f006
Kyungbok Lee <dlrudqhr7180@gmail.com>
2024-03-13 13:36:35 -0400

    add models to init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
 import os
 import sys
 
-from mrdf_margin import MRDF_Margin
-from mrdf_margin_oc import MRDF_Margin_OC
-from mrdf_oc import MRDF_OC
+from models.mrdf_margin import MRDF_Margin
+from models.mrdf_margin_oc import MRDF_Margin_OC
+from models.mrdf_oc import MRDF_OC
